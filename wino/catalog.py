9bd2a8c519852841739b376425fce015e3ca6817
Scott Cruwys <scruwys@gmail.com>
2014-09-08 11:43:17 -0600

    fixed indentation

--- wino/catalog.py
+++ wino/catalog.py
@@ -1,11 +1,12 @@
 from base import Base
 
 class Catalog(Base):
-	def __init__(self, apikey):
-		Base.__init__(self, apikey)
-		self.url = "/".join((self.ENDPOINT, "catalog"))
+	
+    def __init__(self, apikey):
+        Base.__init__(self, apikey)
+       self.url = "/".join((self.ENDPOINT, "catalog"))
 
-	def search(self, string, **params):
-		payload = dict( params.items() + { 'search': string }.items())
-		json = self.get(self.url, params=payload)
-		return json['Products']['List']
+    def search(self, string, **params):
+        payload = dict( params.items() + { 'search': string }.items())
+        json = self.get(self.url, params=payload)
+        return json['Products']['List']
