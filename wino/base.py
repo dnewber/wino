fe08cb0ce6b70b16f8c14964ed7e22b2862a1bbb
Scott Cruwys <scruwys@gmail.com>
2014-09-09 16:59:45 -0600

    added payload method and fixed indentation

--- wino/base.py
+++ wino/base.py
@@ -5,12 +5,15 @@ class Base(object):
     ENDPOINT = 'http://services.wine.com/api/beta2/service.svc/json'
 
     def __init__(self, apikey):
-        self.apikey = { 'apikey': apikey };
+        self.apikey = { 'apikey': apikey }
 
     def get(self, url, **params):
-        payload  = dict(self.apikey.items() + params['params'].items())
+        payload  = self.build_payload(self.apikey, params['params'])
         response = req.get(url, params=payload)
         return response.json()
 
-    def build_payload(self, **params):
-        return
+    def build_payload(self, *params):
+        res = {}
+        for p in params:
+            res.update(p.items())
+        return res
--- wino/catalog.py
+++ wino/catalog.py
@@ -4,9 +4,9 @@ class Catalog(Base):
 	
     def __init__(self, apikey):
         Base.__init__(self, apikey)
-       self.url = "/".join((self.ENDPOINT, "catalog"))
-
+        self.url = "/".join((self.ENDPOINT, "catalog"))
+              	
     def search(self, string, **params):
-        payload = dict( params.items() + { 'search': string }.items())
-        json = self.get(self.url, params=payload)
-        return json['Products']['List']
+        payload  = self.build_payload(params, { 'search': string })
+        response = self.get(self.url, params=payload)
+        return response['Products']['List']
